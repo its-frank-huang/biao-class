e931940bb958f140cc374429a94c96791ab5f0b5
frankie0917 <frankie0917@foxmail.com>
2019-03-09 02:33:32 +0800

    form validate

---
+++ 015-form-validation/validate.js
@@ -0,0 +1,46 @@
+; (function () {
+    'use strict'
+    window.$validate = {
+        isUsername,
+        isEmail,
+        isPhone,
+        isPassword,
+        betweenLength,
+        between
+    };
+
+    function isUsername(username) {
+        if (!betweenLength(username, 4, 20))
+            return false;
+        return true;
+    }
+
+    function isEmail(email) {
+        if (!email.includes('@'))
+            return false;
+        return true;
+    }
+
+    function isPhone(phone) {
+        if ((phone.length != 11 &&
+            phone.length != 13 &&
+            phone.length != 14) ||
+            !phone.startsWith('1'))
+            return false;
+        return true;
+    }
+
+    function isPassword(password) {
+        if (!betweenLength(password, 6, 20))
+            return false;
+        return true;
+    }
+
+    function betweenLength(str, min, max) {
+        return str.length >= min && str.length <= max;
+    }
+
+    function between(num, min, max) {
+        return num >= min && num <= max;
+    }
+})();
