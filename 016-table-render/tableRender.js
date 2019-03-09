aab3b23eb3ac197f131de63d7686fe17dce987aa
frankie0917 <frankie0917@foxmail.com>
2019-03-09 10:32:34 +0800

    table render update

--- 016-table-render/tableRender.js
+++ 016-table-render/tableRender.js
@@ -1,7 +1,7 @@
 ; (function () {
     'use strict'
     //variable prepare
-    let table, struct, data, thead, tbody;
+    let table, struct, data, thead, tbody, operations;
 
     //expose variables to global
     window.$render = { boot };
@@ -11,19 +11,21 @@
      * @param {string} tableSelecter
      * @param {object} structure
      * @param {object} list
+     * @param {object} ops
      */
-    function boot(tableSelecter, structure, list) {
+    function boot(tableSelecter, structure, list, ops) {
         //select table
         table = document.querySelector(tableSelecter);
 
         //create thead,tbody and append to table
         let theadHtml = document.createElement('thead'),
-        tbodyHtml = document.createElement('tbody');
+            tbodyHtml = document.createElement('tbody');
         table.appendChild(theadHtml);
         table.appendChild(tbodyHtml);
         thead = table.tHead;
         tbody = table.tBodies[0];
-        
+        operations = ops;
+
         //update variables
         struct = structure;
         data = list;
@@ -44,15 +46,19 @@
      * @description thead render
      */
     function theadRender() {
+        //prepare html
+        let html = '';
         //for every key in struct
         for (const key in struct) {
-            //create a th element
-            let th = document.createElement('th');
             //input the title of each column e.g. name,phone...
-            th.innerText = struct[key];
-            //append to thead
-            thead.appendChild(th);
+            html += `<th>${struct[key]}</th>`;
         }
+        //if operations then add the operation column
+        if (operations) {
+            html += `<th>操作</th>`
+        }
+        //add to thead
+        thead.innerHTML = html;
     }
 
     /**
@@ -60,20 +66,51 @@
      */
     function tbodyRender() {
         //for each user information 
+        let idCount = 0;
         data.forEach(it => {
+            it.id = idCount;
+            idCount++;
+            //prepare htmls
+            let html = "";
+            let opsHtml = '';
             //create a tr element
             let tr = document.createElement('tr');
             //in each tr...
             for (const key in struct) {
-                //create a td element
-                let td = document.createElement('td');
                 //input each user's detail e.g. frank,number...
-                td.innerText = it[key] || "-";
+                html += `<td>${it[key] || '-'}</td>`;
+            }
+            //put html on tr
+            tr.innerHTML = html;
+            //if operations then create buuttons for operation column
+            if (operations) {
+                //create element
+                opsHtml = document.createElement('td');
                 //append to tr
-                tr.appendChild(td);
+                tr.appendChild(opsHtml);
+                // for every action in operations create a button and bind events to it
+                for (let item in operations) {
+                    //create button html
+                    let button = document.createElement('button');
+                    let action = operations[item];
+                    //add button class
+                    button.classList.add(action.name);
+                    //add button name
+                    button.innerText = action.name;
+                    //append button to td
+                    opsHtml.appendChild(button);
+                    //bind events via class name
+                    tr
+                        .querySelector('.' + action.name)
+                        .addEventListener('click', e => {
+                            //button custom function
+                            action.action(it);
+                        });
+                }
             }
             //append to tbody
             tbody.appendChild(tr);
+            it.tr=tr;
         });
     }
 })();
