1b56f7cf9fafaddf81e48e9fa9819584eae0f7aa
frankie0917 <frankie0917@foxmail.com>
2019-04-15 15:09:12 +0800

    todo practice vue template

--- 023vuetodo/js/main.js
+++ 023vuetodo/js/main.js
@@ -1,19 +1,41 @@
 (function() {
 	'use strict';
 
+	let Event = new Vue();
+
 	function copy(obj) {
 		return Object.assign({}, obj);
 	}
+
+	Vue.component('task', {
+		template : '#task-tpl',
+		props    : [ 'todo' ],
+		methods  : {
+			action(name, params) {
+				Event.$emit(name,params);
+			}
+		}
+	});
 
 	new Vue({
 		el      : '#main',
 		data    : {
 			list    : [],
 			current : {}
-        },
-        mounted(){
-            this.list = ms.get('list') || [];
-        },
+		},
+		mounted() {
+			this.list = ms.get('list') || [];
+			let me = this;
+			Event.$on('remove',params=>{
+				me.remove(params);
+			});
+			Event.$on('set_current',params=>{
+				me.set_current(params);
+			});
+			Event.$on('toggle_complete',params=>{
+				me.toggle_complete(params);
+			});
+		},
 		methods : {
 			merge() {
 				if (this.current.id) {
@@ -26,8 +48,8 @@
 					}
 
 					let todo = { ...this.current };
-                    todo.id = this.next_id();
-                    todo.complete = false;
+					todo.id = this.next_id();
+					todo.complete = false;
 					this.list.push(todo);
 				}
 				this.current = {};
@@ -50,23 +72,23 @@
 				return this.list.findIndex((item) => {
 					return item.id == id;
 				});
-            },
-            toggle_complete(id){
-                let index = this.find_index(id);
-                Vue.set(this.list[index],'complete',!this.list[index].complete);
-            }
+			},
+			toggle_complete(id) {
+				let index = this.find_index(id);
+				Vue.set(this.list[index], 'complete', !this.list[index].complete);
+			}
 		},
 
 		watch   : {
 			list : {
 				deep    : true,
-				handler(n,o) {
-                    if(n){
-                        ms.set('list',n);
-                    }else{
-                        ms.set('list',[]);
-                    }
-                }
+				handler(n, o) {
+					if (n) {
+						ms.set('list', n);
+					} else {
+						ms.set('list', []);
+					}
+				}
 			}
 		}
 	});
