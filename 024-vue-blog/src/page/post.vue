49f3220480bfa9ec01b9b9247d711de7ce986fbd
frankie0917 <frankie0917@foxmail.com>
2019-05-01 12:44:07 +0800

    vue practice comment reply

--- 024-vue-blog/src/page/post.vue
+++ 024-vue-blog/src/page/post.vue
@@ -6,14 +6,17 @@
             <div class="content">{{row.content}}</div>
         </div>
     </div>
-    <div class="card">
-        <div class="card-body">
+    <div class="card mt-3">
+        <div class="card-body ">
             <h2 class="card-title">评论</h2>
             <div class="comment-wrap">
                 <div class="comment-list list-group mb-2">
-                    <div class="comment-item list-group-item" v-for="it in commentList" :key='it'>
-                        <div class="email text-muted">{{it.email}}</div>
-                        <div class="content">{{it.content}}</div>
+                    <div class="comment-item list-group-item clearfix" v-for="it in commentList" :key='it'>
+                        <div class="email text-muted">{{it.email}}
+                            <span v-if="it.reply_to">回复 <span>{{it.$reply_to.email}}</span></span>
+                        </div>
+                        <div class="content float-left">{{it.content}}</div>
+                        <button class="btn btn-light float-right" @click="reply(it.id)">回复</button>
                     </div>
                 </div>
                 <button class="btn btn-secondary" @click="commentFormVisible=!commentFormVisible">评论</button>
@@ -22,7 +25,7 @@
                         <div class="input-group-prepend">
                             <span class="input-group-text">Email</span>
                         </div>
-                        <input class="form-control" type="email" v-model="current.email">
+                        <input class="form-control" id="email" type="email" v-model="current.email">
                     </div>
                     <div class="input-group mt-2">
                         <div class="input-group-prepend">
@@ -71,11 +74,30 @@ export default {
             });
         },
         read(){
-            api('comment/read',{where:{and:{post_id:this.id}}}).then(r=>{
+            let param ={
+                with:[
+                    {
+                        model:'comment',
+                        relation:'belongs_to',
+                        foreign_key:'reply_to',
+                        as:'reply_to'
+                    }
+                ],
+                where:{
+                    and:{
+                        post_id:this.id
+                    }
+                }
+            };
+            api('comment/read',param).then(r=>{
                 if(r.success){
                     this.commentList = r.data;
                 }
             });
+        },
+        reply(id){
+            this.current.reply_to = id;
+            this.commentFormVisible = true;
         },
         createComment(){
             api('comment/create',this.current).then(r=>{
