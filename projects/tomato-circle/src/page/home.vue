327eeadebc09a557455ff2470bec786700ba216d
frankie0917 <frankie0917@foxmail.com>
2019-05-15 16:11:11 +0800

    tomato-circle timeline

--- projects/tomato-circle/src/page/home.vue
+++ projects/tomato-circle/src/page/home.vue
@@ -2,13 +2,21 @@
 	<div class="container">
 		<h1>Home</h1>
 		<form @submit.prevent="createThread" class="card">
-			<h1>分享动态</h1>
-			<input type="text" placeholder="标题" v-model="threadForm.title">
-			<span class="error" v-if="threadError.title">标题不可为空</span>
-			<textarea placeholder="内容" v-model="threadForm.content"></textarea>
-            <button type="submit">提交</button>
+			<fieldset :disabled="pending">
+				<input type="text" placeholder="标题" v-model="threadForm.title">
+				<span class="error" v-if="threadError.title">标题不可为空</span>
+				<textarea placeholder="内容" v-model="threadForm.content"></textarea>
+				<button type="submit">提交</button>
+			</fieldset>
 		</form>
-		<div class="timeline card"></div>
+		<div class="timeline">
+			<div class="activity" v-for="it in threadList">
+				<a class="user">@{{it.$user ? (it.$user.name || it.$user.username): '已注销'}}</a>
+				<div class="title">{{it.title}}</div>
+				<div class="content">{{it.content}}</div>
+				<div class="others">发布于：{{it.created_at}}</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -16,24 +24,42 @@
 import api from "../lib/api";
 import store from "../lib/store";
 import formatter from "../lib/formatter";
+import session from '../lib/session';
 
 export default {
+	mounted() {
+		this.threadRead();
+		console.log(this.threadList);
+		
+	},
 	data() {
 		return {
 			threadForm: {},
+			threadList: [],
 			threadError: {
-				title:false
-			}
+				title: false
+			},
+			pending: false
 		};
 	},
 	methods: {
+		threadRead() {
+			api("thread/read",{
+				with:[
+					'belongs_to:user',
+				],
+			}).then(r => {
+				this.threadList = r.data;
+			});
+		},
 		createThread() {
+			this.pending = true;
 			let form = this.threadForm;
 			let error = this.threadError;
-			
-			
+
 			if (!form.title) {
 				error.title = true;
+				this.pending = false
 				return;
 			}
 
@@ -41,11 +67,20 @@ export default {
 
 			form.cat_id = 1;
 			form.cat_id = 1;
-			
+
+			form.user_id = session.user().id;
+
 			form.created_at = formatter.format(new Date());
 
 			api("thread/create", form).then(r => {
-				console.log(r.data);
+				if (!r.success) {
+					alert("创建失败");
+					return;
+				}
+
+				this.threadForm = {};
+				this.pending = false;
+				this.threadRead();
 			});
 		}
 	}
